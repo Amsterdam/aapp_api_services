80c9e87a2c5d30902759c412edcf90cd59a45962
Stan Syfert <stan@west.nl>
2024-09-17 13:41:07 +0200

    Add swagger paths via core app

--- contact/urls.py
+++ contact/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
 from contact.views import link_views
+from core.urls import get_swagger_paths
 
 
 BASE_PATH = "contact/api/v1"
 
 urlpatterns = [path(BASE_PATH + "/links", link_views.get_links, name="contact-links")]
+urlpatterns += get_swagger_paths(BASE_PATH)
