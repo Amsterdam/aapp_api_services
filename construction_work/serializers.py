a4873de9acb7a51aa56705e307081cc488084432
Stan Syfert <stan@west.nl>
2024-10-09 17:33:06 +0200

    Clean up

--- construction_work/serializers.py
+++ construction_work/serializers.py
@@ -2,7 +2,7 @@ from datetime import timedelta
 
 from django.conf import settings
 from django.utils import timezone
-from drf_spectacular.utils import extend_schema_field, extend_schema_serializer
+from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 from construction_work.models import (
