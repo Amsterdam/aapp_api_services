2fdc039fc684430335d2ab9581ad72d614ab96bf
Haver, Fré <f.haver@amsterdam.nl>
2025-12-02 15:19:22 +0000

    Merged PR 55915: change name of burning guide endpoint
    Affected services
    - bridge
    
    Definition of Done:
    - [ ] API is backwards compatible (`make openapi-diff`)
    - [ ] Swagger UI up-to-date & tested (`make dev`)
    - [ ] Dependencies updated (`make requirements`)
    - [ ] Unittest coverage >90% (`make coverage`)
    - [ ] Infrastructure config updated (aapp_azure_infra)
    - [ ] Loadtests for relevant endpoints (aapp_testing_loadtests)
    
    If manually deployed:
    - [ ] Manual check on dev (o) & test (t), e.g. use feature via Swagger or admin panel
    - [ ] Sanity check on test app
    
    change name of burning guide endpoint

--- bridge/burning_guide/urls.py
+++ bridge/burning_guide/urls.py
@@ -5,7 +5,7 @@ from bridge.burning_guide.views import burning_guide_view
 urlpatterns = [
     # Account
     path(
-        "burning-guide",
+        "burning-guide/api/v1/advice",
         burning_guide_view.BurningGuideView.as_view(),
         name="burning-guide",
     )
