2f470b9cfd35daf9bb26fe21a47c6e2f49649269
jjbeekman <j.beekman@amsterdam.nl>
2026-03-26 14:12:09 +0100

    AM-705 improve entries view (#65)
    Improves how survey entries and related objects are displayed (primarily
    in Django admin) within the survey service.
    
    # Changes:
    
    Tweaks __str__ output for SurveyVersion and SurveyVersionEntry.
    Adds verbose names to several SurveyVersionEntry / Answer fields and
    includes a migration for those schema metadata changes.
    Enhances the admin changelist for SurveyVersionEntry with filters and an
    “antwoorden” (answers) display column.
    
    
    ## Affected services
    - survey
    
    ## Definition of done
    - [ ] API is backwards compatible (`make openapi-diff`)
    - [ ] Swagger UI up-to-date & tested (`make dev`)
    - [ ] Dependencies updated (`make requirements`)
    - [ ] Infrastructure config updated (aapp_azure_infra)
    - [ ] Loadtests for relevant endpoints (aapp_testing_loadtests)
    
    After PR created (and deployed on dev):
    - [ ] Manual check on dev (o) e.g. use feature via Swagger or admin
    panel
    - [ ] Sanity check on test app (check that dev environment is selected)
    
    ## Other notes
    
    GitHub Copilot was used in writing the code

--- survey/admin/survey_entry_admin.py
+++ survey/admin/survey_entry_admin.py
@@ -2,6 +2,7 @@ from adminsortable2.admin import SortableAdminBase
 from django import forms
 from django.contrib import admin
 from django.contrib.admin import TabularInline
+from django.utils.html import format_html_join
 
 from survey.models import Answer, SurveyVersionEntry
 
@@ -19,10 +20,16 @@ class SurveyVersionEntryAdmin(SortableAdminBase, admin.ModelAdmin):
         extra = 0
 
     form = SurveyVersionEntryAdminForm
-    list_display = ["id", "survey_version", "created_at"]
+    list_display = ["id", "survey_version", "created_at", "antwoorden"]
     readonly_fields = ["survey_version", "created_at"]
+    list_filter = ["survey_version__survey__team", "survey_version", "entry_point"]
     inlines = [AnswerInLine]
     ordering = ["-id"]
+
+    def get_queryset(self, request):
+        return SurveyVersionEntry.objects.prefetch_related("answers").select_related(
+            "survey_version__survey"
+        )
 
     def has_add_permission(self, request):
         return False
@@ -32,3 +39,10 @@ class SurveyVersionEntryAdmin(SortableAdminBase, admin.ModelAdmin):
 
     def has_delete_permission(self, request, obj=None):
         return False
+
+    def antwoorden(self, obj):
+        return format_html_join(
+            "\n",
+            "<li>{}: {}</li>",
+            ((a.question.id, a.answer) for a in obj.answers.all()),
+        )
