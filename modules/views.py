5d3754445c9c037d26e12552aa8ca33c9ef36ace
Fré <f.haver@amsterdam.nl>
2026-04-13 11:26:21 +0200

    use get_queryset in get_object

--- modules/views.py
+++ modules/views.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.db.models import Prefetch
 from django.utils.decorators import method_decorator
 from django.views.decorators.cache import cache_page
 from rest_framework import generics, status
@@ -7,7 +8,7 @@ from rest_framework.response import Response
 
 from core.utils.openapi_utils import extend_schema_for_api_key as extend_schema
 from modules.exceptions import ReleaseNotFoundException
-from modules.models import AppRelease
+from modules.models import AppRelease, ReleaseModuleStatus
 from modules.serializers.release_serializers import AppReleaseSerializer
 from modules.utils import VersionQueries
 
@@ -25,15 +26,25 @@ class ReleaseDetailView(generics.RetrieveAPIView):
     lookup_field = "version"
     lookup_url_kwarg = "version"
 
+    def get_queryset(self):
+        prefetch = Prefetch(
+            "releasemodulestatus_set",
+            queryset=ReleaseModuleStatus.objects.select_related(
+                "module_version__module"
+            ).order_by("sort_order"),
+        )
+        return AppRelease.objects.prefetch_related(prefetch)
+
     def get_object(self):
         version = self.kwargs.get(self.lookup_url_kwarg)
+        queryset = self.get_queryset()
         if version == "latest":
-            releases = AppRelease.objects.all()
+            releases = queryset.all()
             if not releases:
                 raise ReleaseNotFoundException
             version = VersionQueries.get_highest_version([x.version for x in releases])
 
-        release = AppRelease.objects.filter(version=version).first()
+        release = queryset.filter(version=version).first()
         if release is None:
             raise ReleaseNotFoundException(
                 f"Release version '{version}' does not exist."
