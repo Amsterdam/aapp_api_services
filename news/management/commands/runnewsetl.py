26a2e9aaee83f17fcc15b2428001046136307030
Fré Haver <f.haver@amsterdam.nl>
2026-05-08 16:05:04 +0200

    lintfix

--- news/management/commands/runnewsetl.py
+++ news/management/commands/runnewsetl.py
@@ -14,8 +14,12 @@ IPROX_ARTICLES_URL = urljoin(IPROX_URL, "item/")
 NEWS_ARTICLE_TYPES = NewsArticle.choices_as_list()
 
 iprox_fetcher = IproxFetcher(
-    iprox_fetch_url=IPROX_URL, iprox_detail_url=IPROX_ARTICLES_URL, sources=NEWS_ARTICLE_TYPES, max_concurrent_requests=20
+    iprox_fetch_url=IPROX_URL,
+    iprox_detail_url=IPROX_ARTICLES_URL,
+    sources=NEWS_ARTICLE_TYPES,
+    max_concurrent_requests=20,
 )
+
 
 class Command(BaseCommand):
     """Upsert news articles"""
@@ -23,13 +27,9 @@ class Command(BaseCommand):
     help = "Upsert news articles"
 
     def handle(self, *args, **kwargs):
-        
+
         # extract all news articles
         logger.info("Extracting news articles from source")
         all_iprox_items = iprox_fetcher.fetch_all_items()
-        extracted_data = iprox_fetcher.fetch_items_data(
-            items=all_iprox_items
-        )
+        extracted_data = iprox_fetcher.fetch_items_data(items=all_iprox_items)
         logger.info(f"Extracted {len(extracted_data)} news articles from source")
-
-
