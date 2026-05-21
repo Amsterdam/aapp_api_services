08e916b44c82fe2c9704f336a332cbf782b68ef6
Jeroen Beekman <j.beekman@amsterdam.nl>
2026-05-21 11:17:15 +0200

    Remove outdated comments

--- bridge/proxy/serializers.py
+++ bridge/proxy/serializers.py
@@ -4,23 +4,17 @@ from core.serializers.address_serializers import AddressSerializer
 
 
 class AddressSearchRequestSerializer(serializers.Serializer):
-    """Serializer for the WasteGuideRequest model."""
-
     q = serializers.CharField(required=False)
     lat = serializers.FloatField(required=False)
     lon = serializers.FloatField(required=False)
 
 
 class AddressSearchByNameSerializer(serializers.Serializer):
-    """Serializer for the WasteGuideRequest model."""
-
     query = serializers.CharField()
     street_name = serializers.CharField(required=False)
 
 
 class AddressSearchByCoordinateSerializer(serializers.Serializer):
-    """Serializer for the WasteGuideRequest model."""
-
     lat = serializers.FloatField()
     lon = serializers.FloatField()
 
