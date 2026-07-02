faa000ec266791b29f1f9ebd32c3ce17c55074cc
Fré Haver <f.haver@amsterdam.nl>
2026-07-02 11:49:52 +0200

    update models test

--- city_pass/tests/test_models.py
+++ city_pass/tests/test_models.py
@@ -46,7 +46,7 @@ class TestTokenModels(TestCase):
         }
     )
     def assert_is_valid_function(self, token_type):
-        token_creation_time = datetime.strptime("2024-01-01 12:00", "%Y-%m-%d %H:%M")
+        token_creation_time = datetime.strptime("2024-01-01 12:00", DATE_FORMAT)
         with freeze_time(token_creation_time):
             token = token_type(session=self.session)
             token.save()
@@ -72,7 +72,7 @@ class TestTokenModels(TestCase):
 
     @override_settings(REFRESH_TOKEN_EXPIRATION_TIME=ONE_HOUR_IN_SECONDS)
     def test_refresh_token_is_expired(self):
-        token_creation_time = datetime.strptime("2024-01-01 12:00", "%Y-%m-%d %H:%M")
+        token_creation_time = datetime.strptime("2024-01-01 12:00", DATE_FORMAT)
         with freeze_time(token_creation_time):
             refresh_token = RefreshToken(session=self.session)
             refresh_token.save()
@@ -84,68 +84,67 @@ class TestTokenModels(TestCase):
             self.assert_token_invalid(refresh_token)
 
     @override_settings(
-        TOKEN_CUT_OFF_DATETIME="08-01 00:00",
+        TOKEN_CUT_OFF_DATETIME="08-01 10:00",
         TOKEN_TTLS={
-            "ACCESS_TOKEN": 30 * 60,
-            "REFRESH_TOKEN": 365 * 24 * 60 * 60,
+            "ACCESS_TOKEN": 4 * 365 * 24 * 60 * 60,  # 4 years in seconds
+            "REFRESH_TOKEN": 4 * 365 * 24 * 60 * 60,  # 4 years in seconds,
         },
     )
     def test_token_created_before_cut_off(self):
-        creation_time_before_cut_off = datetime.fromisoformat(
-            "2026-07-31 23:59:58+02:00"
+        creation_time_before_cut_off = datetime.strptime(
+            "2022-08-01 12:00", DATE_FORMAT
         )
-        with freeze_time(creation_time_before_cut_off):
-            access_token = AccessToken.objects.create(session=self.session)
-            refresh_token = RefreshToken.objects.create(session=self.session)
 
-        with freeze_time(datetime.fromisoformat("2026-07-31 23:59:59+02:00")):
+        with freeze_time(creation_time_before_cut_off):
+            access_token = AccessToken(session=self.session)
+            access_token.save()
+            self.assert_token_valid(access_token)
+
+            refresh_token = RefreshToken(session=self.session)
+            refresh_token.save()
+            self.assert_token_valid(refresh_token)
+
+        before_cut_off_datetime = datetime.strptime("2024-08-01 09:59", DATE_FORMAT)
+        with freeze_time(before_cut_off_datetime):
             self.assert_token_valid(access_token)
             self.assert_token_valid(refresh_token)
 
+            # Check that tokens are still in the database
             self.assertEqual(AccessToken.objects.count(), 1)
             self.assertEqual(RefreshToken.objects.count(), 1)
 
-        with freeze_time(datetime.fromisoformat("2026-08-01 00:00:00+02:00")):
+        after_cut_off_datetime = datetime.strptime("2024-08-01 10:01", DATE_FORMAT)
+        with freeze_time(after_cut_off_datetime):
             self.assert_token_invalid(access_token)
             self.assert_token_invalid(refresh_token)
 
+            # Check that tokens are deleted from the database
             self.assertEqual(AccessToken.objects.count(), 0)
             self.assertEqual(RefreshToken.objects.count(), 0)
 
     @override_settings(
-        TOKEN_CUT_OFF_DATETIME="08-01 00:00",
+        TOKEN_CUT_OFF_DATETIME="08-01 10:00",
         TOKEN_TTLS={
-            "ACCESS_TOKEN": 30 * 60,
-            "REFRESH_TOKEN": 365 * 24 * 60 * 60,
-        },
-    )
-    def test_token_created_at_cut_off_is_valid(self):
-        """
-        Token created at cut off datetime is instantly invalidated.
-        """
-        cut_off_time = datetime.fromisoformat("2026-08-01 00:00:00+02:00")
-
-        with freeze_time(cut_off_time):
-            access_token = AccessToken.objects.create(session=self.session)
-            refresh_token = RefreshToken.objects.create(session=self.session)
-            self.assert_token_invalid(access_token)
-            self.assert_token_invalid(refresh_token)
-
-    @override_settings(
-        TOKEN_CUT_OFF_DATETIME="08-01 00:00",
-        TOKEN_TTLS={
-            "ACCESS_TOKEN": 30 * 60,
-            "REFRESH_TOKEN": 365 * 24 * 60 * 60,
+            "ACCESS_TOKEN": 4 * 365 * 24 * 60 * 60,  # 4 years in seconds
+            "REFRESH_TOKEN": 4 * 365 * 24 * 60 * 60,  # 4 years in seconds,
         },
     )
     def test_token_created_after_cut_off(self):
-        cut_off_time = datetime.fromisoformat("2026-08-01 00:00:01+02:00")
-        with freeze_time(cut_off_time):
-            access_token = AccessToken.objects.create(session=self.session)
-            refresh_token = RefreshToken.objects.create(session=self.session)
+        creation_time_after_cut_off = datetime.strptime("2024-08-01 10:01", DATE_FORMAT)
+        with freeze_time(creation_time_after_cut_off):
+            access_token = AccessToken(session=self.session)
+            access_token.save()
+            self.assert_token_valid(access_token)
 
-        with freeze_time(datetime.fromisoformat("2026-08-01 00:00:02+02:00")):
+            refresh_token = RefreshToken(session=self.session)
+            refresh_token.save()
+            self.assert_token_valid(refresh_token)
+
+        after_cut_off_datetime = datetime.strptime("2024-08-01 10:01", DATE_FORMAT)
+        with freeze_time(after_cut_off_datetime):
             self.assert_token_valid(access_token)
             self.assert_token_valid(refresh_token)
+
+            # Check that tokens are still in the database
             self.assertEqual(AccessToken.objects.count(), 1)
             self.assertEqual(RefreshToken.objects.count(), 1)
