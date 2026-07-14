015a022e8b059dbcb72c46ec4f82a9b891714f3c
Fré Haver <f.haver@amsterdam.nl>
2026-07-14 14:03:10 +0200

    add constants and mock data

--- modules/views/theme_views.py
+++ modules/views/theme_views.py
@@ -2,17 +2,19 @@ import json
 import logging
 
 import httpx
+import requests
 from django.conf import settings
 from django.db.models import Prefetch
 from django.http import StreamingHttpResponse
-from django.utils.decorators import method_decorator
 from django.views import View
-from django.views.decorators.cache import cache_page
 from rest_framework import generics, status
 from rest_framework.response import Response
+from tenacity import retry, retry_if_exception_type, stop_after_attempt, wait_fixed
 
 from core.utils.openapi_utils import custom_extend_schema, extend_schema_for_api_key
+from modules.constants import MAPPING
 from modules.exceptions import ReleaseNotFoundException
+from modules.mock_data import MOCK_MAMS_RESPONSE
 from modules.models import AppRelease, ReleaseModuleStatus
 from modules.serializers.release_serializers import (
     MijnAmsterdamThemesSerializer,
@@ -23,6 +25,10 @@ logger = logging.getLogger(__name__)
 
 
 class MijnAmsterdamThemesStreamView(View):
+    """
+    This view isnt used yet, but is intended to be used for streaming the upstream mijn amsterdam theme stream to the frontend.
+    """
+
     async def get(self, request, *args, **kwargs):
         self.session_id = request.headers.get(settings.MIJN_AMS_HEADER_SESSION_ID)
         return StreamingHttpResponse(
@@ -94,15 +100,64 @@ class MijnAmsterdamThemesView(generics.GenericAPIView):
     @custom_extend_schema(
         success_response=MijnAmsterdamThemesSerializer,
         description=(
-            "Retrieve a specific release. "
-            "When retrieving a release, the response includes the details of the release, including the versions of the modules it consists of. "
-            "The path parameter can be a specific version in the format x.y.z or 'latest' to retrieve the latest release. "
-            "If the specified release does not exist, a ReleaseNotFoundException is raised."
+            "Retrieve the themes of a specific app release. The release is identified by its version number, which is provided as a URL parameter. "
         ),
         default_exceptions=[ReleaseNotFoundException],
     )
-    @method_decorator(cache_page(60))
     def get(self, request, *args, **kwargs):
+
+        access_token = request.headers.get("AccessToken")
+        if not access_token:
+            return Response(
+                {"detail": "Access token is required."},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+
+        # get all the modules in the release
         release = self.get_object()
-        serializer = self.get_serializer(release)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        pre_serializer = self.get_serializer(release)
+        themes = pre_serializer.data.get("themes", [])
+        response_data = (
+            MOCK_MAMS_RESPONSE  # Replace with actual response from the upstream service
+        )
+
+        relevant_themes = []
+
+        for module_slug, fields in MAPPING.items():
+            print("Theme module slug:", module_slug)
+            # check if the module is in the release
+            theme = [x for x in themes if x["moduleSlug"] == module_slug]
+            if theme:
+                print(f"Module {module_slug} is in the release.")
+                content = {}
+                for field in fields:
+                    response_content = response_data.get(field, None)
+                    if response_content:  # check if the field is present in the mock data, empty lists are considered invalid
+                        content[field] = response_content
+
+                if content:
+                    # if there is content, add the module (all module version info) and the content to the relevant themes list
+                    relevant_themes.append({**theme[0], "content": content})
+
+        print(f"Retrieved {len(themes)} themes for release version {release.version}")
+
+        return Response({"themes": relevant_themes}, status=status.HTTP_200_OK)
+
+    @retry(
+        stop=stop_after_attempt(3),
+        wait=wait_fixed(2),
+        retry=retry_if_exception_type(requests.exceptions.RequestException),
+        reraise=True,  # Reraise the RequestException after retries
+    )
+    def _make_request(self, access_token, headers) -> requests.Response:
+        """Make the HTTP request for toilet data with retries and a timeout."""
+        url = settings.MIJN_AMS_API_DOMAIN + settings.MIJN_AMS_API_PATHS["ALL"]
+        try:
+            response = requests.get(
+                url, cookies={"access_token": access_token}, headers=headers, timeout=10
+            )
+            response.raise_for_status()
+            return response
+        except requests.exceptions.RequestException:
+            logger.info("Failed to fetch data", extra={"url": url})
+            raise
