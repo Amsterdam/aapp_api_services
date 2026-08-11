fac9dab854d5890c8d22f4947ff3c5f7bdb9aa64
Fré Haver <f.haver@amsterdam.nl>
2026-08-11 13:39:46 +0200

    add pass type to passes response

---
+++ city_pass/tests/mock_data/aanbieding_transactions.py
@@ -0,0 +1,26 @@
+MOCK_DATA = {
+    "discountAmountTotal": 150,
+    "discountAmountTotalFormatted": "€150,0",
+    "transactions": [
+        {
+            "id": "201606",
+            "title": "AFC Ijburg",
+            "description": "Schrijf je in voor een proefles bij AFC Ijburg",
+            "discountTitle": "Gratis",
+            "discountAmount": 100,
+            "discountAmountFormatted": "€100,0",
+            "datePublished": "2025-07-28T21:59:59.000Z",
+            "datePublishedFormatted": "28 juli 2025",
+        },
+        {
+            "id": "201607",
+            "title": "Hema",
+            "description": "Koop een leuke knuffel bij Hema",
+            "discountTitle": "50 euro korting",
+            "discountAmount": 50,
+            "discountAmountFormatted": "€50,0",
+            "datePublished": "2025-07-31T21:59:59.000Z",
+            "datePublishedFormatted": "31 juli 2025",
+        },
+    ],
+}
